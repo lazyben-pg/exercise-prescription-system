fba1ae218270a70b446a8f125ba90e6123b4fe76
lazyben <458861463@qq.com>
2021-12-22 14:07:36 +0800

    解决跨域问题

--- src/main/java/com/lazyben/exercise/config/CorsConfig.java
+++ src/main/java/com/lazyben/exercise/config/CorsConfig.java
@@ -1,129 +1,27 @@
 package com.lazyben.exercise.config;
 
-import org.springframework.boot.autoconfigure.session.DefaultCookieSerializerCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.session.MapSessionRepository;
-import org.springframework.session.SessionRepository;
-import org.springframework.session.config.annotation.web.http.EnableSpringHttpSession;
-import org.springframework.session.web.http.DefaultCookieSerializer;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-import java.util.Arrays;
-import java.util.concurrent.ConcurrentHashMap;
-
-
-//@Configuration
-//public class CorsConfig {
-//    private CorsConfiguration buildConfig() {
-//        CorsConfiguration corsConfiguration = new CorsConfiguration();
-//        corsConfiguration.addAllowedOriginPattern("*"); // 1允许任何域名使用
-//        corsConfiguration.addAllowedHeader("*"); // 2允许任何头
-//        corsConfiguration.addAllowedMethod("*"); // 3允许任何方法（post、get等）
-//        corsConfiguration.setAllowCredentials(true);//支持安全证书。跨域携带cookie需要配置这个
-//        corsConfiguration.setMaxAge(3600L);//预检请求的有效期，单位为秒。设置maxage，可以避免每次都发出预检请求
-//        return corsConfiguration;
-//    }
-//
-//    @Bean
-//    public CorsFilter corsFilter() {
-//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//        source.registerCorsConfiguration("/**", buildConfig()); // 4
-//        return new CorsFilter(source);
-//    }
-//}
-
-//@Configuration
-//public class CorsConfig {
-//    @Bean
-//    public WebMvcConfigurer corsConfigurer() {
-//        return new WebMvcConfigurer() {
-//            @Override
-//            public void addCorsMappings(CorsRegistry registry) {
-//
-//                registry.addMapping("/**")
-//                        .allowedHeaders("*")
-//                        .allowedMethods("*")
-////                        .allowedOrigins("**");
-//                        .allowedOriginPatterns("*")
-//                        .allowCredentials(true);
-//            }
-//        };
-//    }
-//
-//}
 
 @Configuration
-@EnableSpringHttpSession
-public class CorsConfig implements WebMvcConfigurer {
-
+public class CorsConfig {
     @Bean
-    public SessionRepository sessionRepository() {
-        return new MapSessionRepository(new ConcurrentHashMap<>());
-    }
-
-    @Bean
-    DefaultCookieSerializerCustomizer cookieSerializerCustomizer() {
-        return new DefaultCookieSerializerCustomizer() {
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
             @Override
-            public void customize(DefaultCookieSerializer cookieSerializer) {
-                cookieSerializer.setSameSite("None");
-//                cookieSerializer.setUseSecureCookie(true); // 此项必须，否则set-cookie会被chrome浏览器阻拦
+            public void addCorsMappings(CorsRegistry registry) {
+
+                registry.addMapping("/**")
+                        .allowedHeaders("*")
+                        .allowedMethods("*")
+                        .allowedOriginPatterns("*")
+                        .allowCredentials(true);
             }
         };
-    }
-
-    /**
-     * 页面跨域访问Controller过滤
-     *
-     * @return
-     */
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        WebMvcConfigurer.super.addCorsMappings(registry);
-//        registry.addMapping("/**")
-//                .allowCredentials(true)
-//                .allowedHeaders("*")
-//                .allowedMethods("*")
-////                .allowedOrigins("*");
-//                .allowedOriginPatterns("*");
-//    }
-
-    @Bean
-    public CorsFilter corsFilter() {
-        final UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
-        final CorsConfiguration corsConfiguration = new CorsConfiguration();
-        /* Allow cookies */
-        corsConfiguration.setAllowCredentials(true);
-        /* This is your javascript origin */
-        corsConfiguration.addAllowedOriginPattern("*");
-        corsConfiguration.addAllowedHeader("*");
-        corsConfiguration.addAllowedMethod("*");
-        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
-        return new CorsFilter(urlBasedCorsConfigurationSource);
-
     }
 }
 
 
-//@Configuration
-//public class CorsConfig {
-//    @Bean
-//    public WebMvcConfigurer corsConfigurer() {
-//        return new WebMvcConfigurer() {
-//            @Override
-//            public void addCorsMappings(CorsRegistry registry) {
-//                registry.addMapping("/**").allowedOrigins("http://localhost:8081");
-//            }
-//        };
-//    }
-//}
